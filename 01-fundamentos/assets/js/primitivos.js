8bb9f120d7902c1881e341241204c2fceeaae82d
Javier Ramos <jaramosperez85@gmail.com>
2020-08-20 22:34:54 -0400

    primitivos

---
+++ 01-fundamentos/assets/js/primitivos.js
@@ -0,0 +1,35 @@
+let nombre = 'Bruce Wayne';
+console.log( nombre );
+
+nombre = 'Bruno Diaz';
+console.log( nombre );
+
+nombre = "Tía May";
+nombre = `Tía May`; 
+console.log( nombre );
+
+console.log( typeof nombre );
+
+nombre = 123;
+console.log( typeof nombre );
+
+let isMarvel = true;
+console.log( isMarvel );
+
+console.log( typeof isMarvel );
+
+let numero = 123;
+console.log( typeof numero );
+
+let superPorder;
+console.log( typeof superPoder );
+
+let isNull = null;
+console.log( typeof isNull );
+
+let Symbol1 = Symbol('a');
+let Symbol2 = Symbol('a');
+
+console.log( typeof Symbol1 );
+console.log( typeof Symbol2 );
+console.log( Symbol1 === Symbol2 );
