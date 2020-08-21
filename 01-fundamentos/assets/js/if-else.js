e19123f8b77cab03573f16031231f4cf99fcb5f1
Javier Ramos <jaramosperez85@gmail.com>
2020-08-21 17:45:13 -0400

    agregando otra forma

--- 01-fundamentos/assets/js/if-else.js
+++ 01-fundamentos/assets/js/if-else.js
@@ -23,4 +23,19 @@ if (dia === 5) {
     console.log('El día es viernes');
 }
 
-//
+// Sin usar If Else, o Switch, únicamente objetos
+dia = 3; // 0:domingo, 1: lunes....
+
+// const diaSemana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+const diaSemana = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miercoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sabado',
+}
+
+
+console.log( diaSemana[dia] || 'Día no definido');
