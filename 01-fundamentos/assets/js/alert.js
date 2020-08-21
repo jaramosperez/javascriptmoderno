3884ddfa2fe38d9b63da83812b6024ea39c04469
Javier Ramos <jaramosperez85@gmail.com>
2020-08-20 22:01:09 -0400

    first commit

---
+++ 01-fundamentos/assets/js/alert.js
@@ -0,0 +1,8 @@
+// alert('Hola Mundo');
+
+// let nombre = prompt('¿Cuál es tu nombre?', 'Sin nombre');
+
+// seleccion = confirm('¿Está seguro de borrar esto?');
+// console.log( seleccion )
+
+console.log( global );
---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,31 @@
+
+
+
+
+// console.log( console.log('Hola Mundo') );
+
+// let a = 10;
+// var b = 10;
+// const c = 10;
+
+// c = 20;  // Esto no se puede hacer.
+
+let a = 10, b = 20, c = 'Hola ', d = 'Batman'; x = a + b;
+
+const saludo = c + d
+
+
+// console.log('%c Mis Variables', 'color:blue; font-weight: bold');
+// console.log( {a} );
+// console.log( {b} );
+// console.log( {c} );
+
+c = 'Hola Mundo de nuevo!';
+
+
+// console.table({a, b, c, d, x});
+
+// let outerWidth  = 1000000;
+// const outerHeight = 600;
+ 
+let miNuevoNombre = 'Bruce Wayne';
