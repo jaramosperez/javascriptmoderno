45fdda5ec5c258fe184536426c83db9ee7f2f45c
Javier Ramos <jaramosperez85@gmail.com>
2020-08-21 01:53:58 -0400

    Funciones tradicionales, anonimas, flecha.

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -2,54 +2,79 @@
 function saludar() {
     console.log('Hola Mundo');   
 }
-
-saludar();
-
+// saludar();
 //
 // Funciones Anónimas.
 const saludar2 = function() {
     console.log('Hola Mundo!');
 }
-
-
-saludar2();
+// saludar2();
 //
-
 const saludar3 = function( nombre ) {
     console.log('Hola ' + nombre);
 }
-
-saludar3('Javier');
+// saludar3('Javier');
 //
 const saludar4 = function( nombre ) {
     console.log('Hola '+ nombre+ '!');
 }
-saludar4( 'Javier' );
+// saludar4( 'Javier' );
 
 function saludar5(nombre) {
     console.log('Hola '+ nombre);
 }
-
-saludar5('Javier')
-
+// saludar5('Javier')
 // Esto solo funciona en las funciones tradicionales.
 function saludar6( nombre ) {
     console.log( arguments );
-    console.log('Hola Mundo');   
-}
+    console.log('Hola Mundo');  
+    return 10; 
 
-saludar6('Javier', 'atila', 123, true);
+    //Esto nunca se va a  ejecutar.
+    console.log('Soy un código que no se ejecutará');
+}
+// const retornoDeSaludar = saludar6( 'Javier' );
+// saludar6( 'Javier', 'atila', 123, true );
+// console.log( retornoDeSaludar );
 
 //  Funciones de flecha.
 const saludarFlecha = () => {
     console.log('Hola Mundo desde Javascript!');
 }
-
-saludarFlecha();
+// saludarFlecha();
 
 // Cuando se pasan los argumentos no es necesario agregar los parentesis.
 const saludarFlecha2 = ( nombre ) => {
     console.log('Hola '+ nombre);
 }
 
-saludarFlecha2('Javito');
+// saludarFlecha2('Javito');
+
+// Función Tradicional
+function sumar(a, b) {
+    return a + b;
+}
+console.log( sumar(1,2) );
+
+// Como función de felcha.
+const sumar2 = (a,b) => {
+    return a + b;
+}
+console.log(sumar2(2,5))
+
+// Como función de flecha resumida cuando solo es una linea.
+const sumar3 = (a,b) => a + b;
+
+console.log(sumar3(3,1));
+
+//
+function getAleatorio(){
+    return Math.random();
+}
+
+console.log( getAleatorio() );
+
+//  Tarea  transformar la funcion anterior a function de flecha en una linea.
+
+const numeroAleatorio = () => Math.random();
+console.log( numeroAleatorio() )
