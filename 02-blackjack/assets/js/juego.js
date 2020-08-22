c696fd285ccef2b7085d66573f9d9efb527e477f
Javier Ramos <jaramosperez85@gmail.com>
2020-08-22 14:51:49 -0400

    Barajador de cartas creado

---
+++ 02-blackjack/assets/js/juego.js
@@ -0,0 +1,32 @@
+/**
+ * 2C = Two of Clubs (Tréboles)
+ * 2D = Two of Diaminds (Diamantes)
+ * 2H = Two of Hearts (Corazones)
+ * 2S = Two of Spades (Espadas)
+ */
+
+ let deck         = [];
+
+ const tipos      = ['C','D','H','S'];
+ const especiales = ['A','J','Q','K'];
+
+ const crearDeck = () => {
+     for( let i = 2; i<= 10 ; i++ ){
+         for( let tipo of tipos ) {
+            deck.push( i + tipo );
+         }
+     }
+            // Ingreso de Cartas especiales.
+     for(let tipo of tipos) {
+         for( let esp of especiales ) {
+             deck.push(esp + tipo);
+         }
+     }
+
+     console.log( deck );
+     deck = _.shuffle(deck); // Baraja los elementos al azar.
+     console.log(deck)
+     return deck;
+ }
+
+ crearDeck();
