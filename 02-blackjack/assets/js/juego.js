9dcca06e6a480573f66249a9792ff19be2d3e36c
Javier Ramos <jaramosperez85@gmail.com>
2020-08-22 15:17:12 -0400

    metodo para pedir carta y removerla

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -5,28 +5,42 @@
  * 2S = Two of Spades (Espadas)
  */
 
- let deck         = [];
+let deck = [];
 
- const tipos      = ['C','D','H','S'];
- const especiales = ['A','J','Q','K'];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
 
- const crearDeck = () => {
-     for( let i = 2; i<= 10 ; i++ ){
-         for( let tipo of tipos ) {
-            deck.push( i + tipo );
-         }
-     }
-            // Ingreso de Cartas especiales.
-     for(let tipo of tipos) {
-         for( let esp of especiales ) {
-             deck.push(esp + tipo);
-         }
-     }
+// Esta función crea un nuevo Deck.
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo);
+        }
+    }
+    // Ingreso de Cartas especiales.
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(esp + tipo);
+        }
+    }
+    //console.log( deck ); // Ordenado
+    deck = _.shuffle(deck); // Baraja los elementos al azar.
+    console.log(deck)
+    return deck;
+}
 
-     console.log( deck );
-     deck = _.shuffle(deck); // Baraja los elementos al azar.
-     console.log(deck)
-     return deck;
- }
+crearDeck();
 
- crearDeck();
+
+// Esta Función me permite tomar una carta
+const pedirCarta = () => {
+    if( deck.length === 0 ){
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    console.log(deck); // deck sin la carta en la baraja.
+    console.log(carta); // Carta debe ser de la baraja
+    return carta;
+}
+
+pedirCarta();
