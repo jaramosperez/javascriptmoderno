d54bb4e490549d8ca4f2ba7e261333bdbe3eb2a9
Javier Ramos <jaramosperez85@gmail.com>
2020-08-22 22:45:59 -0400

    Métodos en clases

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -11,9 +11,23 @@ class Persona {
         this.codigo = codigo;
         this.frase  = frase;
     }
+
+    // Métodos
+    quienSoy(){
+        console.log(`soy ${ this.nombre } y mi identidad es ${ this.codigo }`);
+    }
+
+    miFrase() {
+        console.log(`${ this.codigo } dice: ${ this.frase }`);
+    }
 }
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Yo soy tu amigable vecino spiderman');
 const ironman   = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman');
 console.log( spiderman );
 console.log( ironman );
+
+spiderman.quienSoy();
+spiderman.miFrase();
+ironman.quienSoy();
+ironman.miFrase();
