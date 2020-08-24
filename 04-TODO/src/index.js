d0f05644a6a5c7e8fe01bf97933ad3adb70d7d9f
Javier Ramos <jaramosperez85@gmail.com>
2020-08-23 22:08:06 -0400

    Completando TODO

--- 04-TODO/src/index.js
+++ 04-TODO/src/index.js
@@ -1,7 +1,8 @@
-import { saludar } from './js/componentes.js';
 import './styles.css';
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
+export const todoList = new TodoList();
 
-const nombre = 'Fernando';
-
-saludar( nombre );
+todoList.todos.forEach( todo => crearTodoHtml( todo ) );
+// todoList.todos.forEach( crearTodoHtml);
